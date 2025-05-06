ba803dbfaa718a49ff7920754ea239039fea4575
Nathan <nathan162318@gmail.com>
2025-05-06 12:16:13 +0200

    Delete Fr directory

--- Fr/js/load.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", function () {
-            setTimeout(function () {
-                document.getElementById('loader-container').style.display = 'none';
-                const content = document.getElementById('content');
-                content.style.display = 'block';
-            }, 3000);
-        });
